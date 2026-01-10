3d1f7ccee0666403d8d674c9287b3e084e3475cd
ferdaws-ahmed <mdferdawsahmeed@gmail.com>
2026-01-10 14:41:58 +0600

    add problem 4 solution

--- index.js
+++ index.js
@@ -101,7 +101,19 @@ console.log(isPalindrome("hello")); // Output: false
 
 // ====================================== Solution ======================================
 
+function findMax(arr) {
+    let max = arr[0];
 
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i]; 
+        }
+    }
+    return max;
+}
+
+const input = [5, 1, 9, 3];
+console.log(findMax(input)); 
 
 
 
